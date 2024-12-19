db984c79b0b0030cec912a6c192223e4a35bc505
Tim <tim@heidelba.ch>
2024-12-19 20:47:16 +0100

    Initial commit

---
+++ src/main/java/dev/tim9h/collections/LimitedIterableStack.java
@@ -0,0 +1,124 @@
+package dev.tim9h.collections;
+
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class LimitedIterableStack<T> extends LinkedList<T> {
+
+	private static final long serialVersionUID = 1L;
+
+	private int capacity;
+
+	private int cursor = 0;
+
+	public LimitedIterableStack(int capacity) {
+		this.capacity = capacity;
+	}
+
+	@Override
+	public void push(T value) {
+		add(value);
+		if (size() > capacity) {
+			removeFirst();
+		}
+		cursor = 0;
+	}
+
+	@Override
+	public T pop() {
+		if (isEmpty()) {
+			return null;
+		}
+		var value = getLast();
+		removeLast();
+		return value;
+	}
+
+	@Override
+	public T peek() {
+		if (isEmpty()) {
+			return null;
+		}
+		return getLast();
+	}
+
+	public T previous() {
+		cursor++;
+		if (isEmpty()) {
+			cursor = 0;
+			return null;
+		} else if (size() - cursor < 0) {
+			cursor = 1;
+			return getLast();
+		}
+		return get(size() - cursor);
+	}
+
+	public T next() {
+		cursor--;
+		if (isEmpty()) {
+			cursor = 0;
+			return null;
+		} else if (cursor <= 0) {
+			cursor = size();
+			return getFirst();
+		}
+		return get(size() - cursor);
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+		if (capacity < size() && capacity > 0) {
+			removeRange(0, size() - capacity);
+			if (cursor > capacity) {
+				cursor = 0;
+			}
+		} else if (capacity <= 0) {
+			clear();
+			this.capacity = 0;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return isEmpty() ? "[empty]" : "[" + stream().map(t -> {
+			if (cursor > 0 && t == get(size() - cursor)) {
+				return "(" + t.toString() + ")";
+			} else {
+				return t.toString();
+			}
+		}).collect(Collectors.joining(", ")) + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		var result = super.hashCode();
+		return 31 * result + Objects.hash(capacity, cursor);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		var other = (LimitedIterableStack<?>) obj;
+		return capacity == other.capacity && cursor == other.cursor;
+	}
+	
+	public int getCursor() {
+		return cursor;
+	}
+	
+	public T getCursorValue() {
+		return get(size() - cursor -1);
+	}
+
+}
---
+++ src/test/java/dev/tim9h/collections/LimitedIterableStackTest.java
@@ -0,0 +1,117 @@
+package dev.tim9h.collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class LimitedIterableStackTest {
+	
+	@Test
+	void testCapacity() {
+		var capacity = 5;
+		var stack = new LimitedIterableStack<Integer>(capacity);
+		for (var i = 0; i < 10; i++) {
+			stack.push(i);
+		}
+		assertEquals(5, stack.size());
+	}
+
+	@Test
+	void testCapacityIncrease() {
+		var capacity = 5;
+		var stack = new LimitedIterableStack<Integer>(capacity);
+		for (var i = 0; i < 10; i++) {
+			stack.push(i);
+		}
+		stack.setCapacity(8);
+		for (var i = 10; i < 20; i++) {
+			stack.push(i);
+		}
+		assertEquals(8, stack.size());
+		assertEquals(12, stack.getFirst());
+		assertEquals(19, stack.getLast());
+	}
+
+	@Test
+	void testCapacityDecrease() {
+		var capacity = 5;
+		var stack = new LimitedIterableStack<Integer>(capacity);
+		for (var i = 0; i < 10; i++) {
+			stack.push(i);
+		}
+		stack.setCapacity(3);
+		assertEquals(3, stack.size());
+		assertEquals(7, stack.getFirst());
+		assertEquals(9, stack.getLast());
+	}
+
+	@Test
+	void testPop() {
+		var stack = new LimitedIterableStack<Integer>(3);
+		for (var i = 0; i < 2; i++) {
+			stack.push(i);
+		}
+		assertEquals(1, stack.pop());
+		assertEquals(1, stack.size());
+		stack.pop();
+		assertTrue(stack.isEmpty());
+		assertNull(stack.pop());
+	}
+
+	@Test
+	void testPrevious() {
+		var stack = new LimitedIterableStack<Integer>(3);
+		for (var i = 0; i < 3; i++) {
+			stack.push(i);
+		}
+		assertEquals(0, stack.getCursor());
+		assertEquals(2, stack.previous());
+		assertEquals(1, stack.getCursor());
+		assertEquals(1, stack.previous());
+		assertEquals(2, stack.getCursor());
+		assertEquals(0, stack.previous());
+		assertEquals(2, stack.previous());
+		assertEquals(1, stack.getCursor());
+		stack.setCapacity(0);
+		assertNull(stack.previous());
+	}
+	
+	@Test
+	void testNext() {
+		var stack = new LimitedIterableStack<Integer>(3);
+		for (var i = 0; i < 3; i++) {
+			stack.push(i);
+		}
+		assertEquals(0, stack.getCursor());
+		assertEquals(0, stack.next());
+		assertEquals(3, stack.getCursor());
+		assertEquals(1, stack.next());
+		assertEquals(2, stack.getCursor());
+		assertEquals(2, stack.next());
+		assertEquals(1, stack.getCursor());
+		assertEquals(0, stack.next());
+		assertEquals(3, stack.getCursor());
+		stack.setCapacity(0);
+		assertNull(stack.next());
+	}
+	
+	@Test
+	void testCapacityCursorMovement() {
+		var stack = new LimitedIterableStack<Integer>(5);
+		for (var i = 0; i < 5; i++) {
+			stack.push(i);
+		}
+		stack.previous();
+		stack.previous();
+		assertEquals(2, stack.getCursor());
+		stack.setCapacity(4);
+		stack.previous();
+		stack.previous();
+		stack.setCapacity(3);
+		assertEquals(0, stack.getCursor());
+		
+	}
+
+}
